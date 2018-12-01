de62db80cff38fff504c5b13614a47061259baba
Max <mctestpub@gmail.com>
2018-12-01 13:47:55 +0200

    reshapre menus, add lodash, axios

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 "use strict";
 
 const TelegramBot = require('node-telegram-bot-api');
+const axios = require('axios');
 
 // replace the value below with the Telegram token you receive from @BotFather
 const token ="509757534:AAFs9jUqQrRZsZ6bYqGFhXKoG1Bk1yYWYV0";
+const appKey = "601a275a6193c68473a0c215f7c12a06";
+const appId = "1c8f4af7";
+const baseURL = "https://api.weatherunlocked.com/"
+const resortsId = {
+  "france": {
+    "valThorens": "333020"
+  },  
+  gudauri: "54888031"
+}
+
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
@@ -43,25 +54,59 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
 
 // Listen for any kind of message. There are different kinds of
 // messages.
+// bot.on('message', (msg) => {
+//   var tempString = "gudauri";
+//   var summary =""
+//   CreateDays(); // creats days string for "bot.sendmessage" - created to see if its works
+//   const chatId = msg.chat.id;
+//   if(msg.text.toString().toLowerCase().indexOf(tempString) === 0 ){
+//     //סיכום מסויים של שלג.. לזה התכוונת? אם לא לא נורא חחח
+//     if(gudauri.amountOfSnow == 0)
+//       summary ="אין שלג";
+//     if(gudauri.amountOfSnow >0 && gudauri.amountOfSnow <6)
+//       summary = "שלג בינוני";
+//     else
+//       summary = "שלג כבד";
+//     bot.sendMessage(chatId,"שם אתר: " + gudauri.placeName + "\nעדכון אחרון: " + gudauri.lastUpdate + "\nטמפרטורה: " + gudauri.temperature + "\n\nסיכום: " + summary + "\n\nשלג (שבוע מהיום) " + daysString);
+//   }
+//   //console.log(msg.text.toString());
+//   // send a message to the chat acknowledging receipt of their message
+//   //bot.sendMessage(chatId, 'Received your message ' + msg.text.toString());
+// });
 bot.on('message', (msg) => {
-  var tempString = "gudauri";
-  var summary =""
-  CreateDays(); // creats days string for "bot.sendmessage" - created to see if its works
-  const chatId = msg.chat.id;
-  if(msg.text.toString().toLowerCase().indexOf(tempString) === 0 ){
-    //סיכום מסויים של שלג.. לזה התכוונת? אם לא לא נורא חחח
-    if(gudauri.amountOfSnow == 0)
-      summary ="אין שלג";
-    if(gudauri.amountOfSnow >0 && gudauri.amountOfSnow <6)
-      summary = "שלג בינוני";
-    else
-      summary = "שלג כבד";
-    bot.sendMessage(chatId,"שם אתר: " + gudauri.placeName + "\nעדכון אחרון: " + gudauri.lastUpdate + "\nטמפרטורה: " + gudauri.temperature + "\n\nסיכום: " + summary + "\n\nשלג (שבוע מהיום) " + daysString);
+  var forecast = "Snow Forecast";
+  if (msg.text.indexOf(forecast) === 0) {
+      bot.sendMessage(msg.chat.id, "Select a country:", {
+        "reply_markup": {
+          "keyboard": [["France"], , ["Back"]]
+        }
+      });
   }
-  //console.log(msg.text.toString());
-  // send a message to the chat acknowledging receipt of their message
-  //bot.sendMessage(chatId, 'Received your message ' + msg.text.toString());
+  var France = "France";
+  if (msg.text.indexOf(France) === 0) {
+      bot.sendMessage(msg.chat.id, "Select a resort", {
+        "reply_markup": {
+          "keyboard": [["Val Thorens"], ["Back"]] //list all available resorts 
+        }
+      });
+  } 
+  var valThorens = "Val Thorens";
+  if (msg.text.indexOf(valThorens) === 0) {
+    bot.sendMessage(msg.chat.id);
+  }
+
 });
+
+
+bot.onText(/\/start/, (msg) => {
+    
+  bot.sendMessage(msg.chat.id, "Welcome to snowbot, please start by choosing a county to view its resorts (TODO)", {
+  "reply_markup": {
+      "keyboard": [["France"], ["Sample text", "Second sample"], ["Back"]]
+      }
+  });
+      
+  });
 
 function CreateDays(){
   JSON.stringify(days);
