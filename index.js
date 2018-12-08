f3c608135eefa7ff6bcfa19fb5ed11208e7d48fb
DannyGusev <dannygu555@gmail.com>
2018-12-08 22:58:51 +0200

    response bot string added, function "CreateDays" added

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 const TelegramBot = require('node-telegram-bot-api');
 const axios = require('axios');
 
+var data;
+
 // replace the value below with the Telegram token you receive from @BotFather
 const token ="509757534:AAFs9jUqQrRZsZ6bYqGFhXKoG1Bk1yYWYV0";
 const appKey = "601a275a6193c68473a0c215f7c12a06";
@@ -65,28 +67,34 @@ bot.on('message', (msg) => {
     axios.get(baseURL+"api/resortforecast/"+resortsId.france.valThorens+apiSuffix, {})
     .then((response) => {
       console.log(response.data);
-      bot.sendMessage(msg.chat.id, response.data.toString())
+      data = response.data;
+      CreateDays();
+      bot.sendMessage(msg.chat.id, "אתר: " + response.data.name + "\n" +
+                                   "מדינה: " + response.data.country + "\n" +
+                                   "תחזית לתאריכים " + response.data.forecast[response.data.forecast.length-1].date + " - " + response.data.forecast[0].date + " : \n"  +
+                                   daysString)
     })
     .catch((error) => {
 
     })
-    var report = "Sno Report";
+  }
+    var report = "Snow Report";
     if(msg.text.indexOf(report) === 0) {
       axios.get(baseURL+"api/snowreport/"+resortsId.france.valThorens+apiSuffix, {})
     .then((response) => {
       console.log(response.data);
-      bot.sendMessage(msg.chat.id, response.data.toString())
+      bot.sendMessage(msg.chat.id, response.data.resortid.toString())
     })
     .catch((error) => {
 
     })
-    }
+   }
       // bot.sendMessage(msg.chat.id, "Select a country:", {
       //   "reply_markup": {
       //     "keyboard": [["France"], KeyBoards.Back ]
       //   }
       // });
-  }
+  
 
   var France = "France";
   if (msg.text.indexOf(France) === 0) {
@@ -119,13 +127,46 @@ bot.onText(/\/start/, (msg) => {
   });
       
   });
-
+var morning = "10:00";
+var night = "22:00";
 function CreateDays(){
-  JSON.stringify(days);
+  //JSON.stringify(days);
   daysString = "";
-  for(var j = 0 ; j < days.length ; j++){
-    daysString += "\n [" + days[j].date + "] - [" + days[j].snow + "] סמ ";
+  for(var j = 0 ; j < data.forecast.length; j++){
+    //daysString += data.forecast.date + " \n ";
+    if(data.forecast[j].time.toString() === morning)
+    {
+      daysString += "------------------------------------------------------------";
+      daysString += "\n";
+      daysString += "תאריך : " + data.forecast[j].date.toString() + " \n";
+      daysString += "זמן : " + data.forecast[j].time.toString() + " \n";
+      daysString += "שלג (ממ) : " + data.forecast[j].snow_mm.toString() + " \n";
+      daysString += "שלג (אינצ') : " + data.forecast[j].snow_in.toString() + " \n";
+      daysString += "גשם (ממ) : " + data.forecast[j].rain_mm.toString() + " \n";
+      daysString += "גשם (אינצ') : " + data.forecast[j].rain_in.toString() + " \n";
+      daysString += "לחות : " + data.forecast[j].hum_pct.toString() + "% \n";
+      daysString += "מהירות רוח (קילומטר לשעה) : " + data.forecast[j].vis_km.toString() + " \n";
+      daysString += "מהירות רוח (מייל לשעה) : " + data.forecast[j].vis_mi.toString() + " \n";
+      daysString += "\n";
+    }
+    if(data.forecast[j].time.toString() === night)
+    {
+      daysString += "תאריך : " + data.forecast[j].date.toString() + " \n";
+      daysString += "זמן : " + data.forecast[j].time.toString() + " \n";
+      daysString += "שלג (ממ) : " + data.forecast[j].snow_mm.toString() + " \n";
+      daysString += "שלג (אינצ') : " + data.forecast[j].snow_in.toString() + " \n";
+      daysString += "גשם (ממ) : " + data.forecast[j].rain_mm.toString() + " \n";
+      daysString += "גשם (אינצ') : " + data.forecast[j].rain_in.toString() + " \n";
+      daysString += "לחות : " + data.forecast[j].hum_pct.toString() + "% \n";
+      daysString += "מהירות רוח (קילומטר לשעה) : " + data.forecast[j].vis_km.toString() + " \n";
+      daysString += "מהירות רוח (מייל לשעה) : " + data.forecast[j].vis_mi.toString() + " \n ";
+      //daysString += "-----------------------"
+    }
+
+    //"\n [" + days[j].date + "] - [" + days[j].snow + "] סמ ";
+    
   }
+  //console.log(daysString);
 }
 bot.on('webhook_error', (error) => {
   console.log(error.code);  // => 'EPARSE'
